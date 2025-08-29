dbacc36c7f00c5a87cbf3a475ff29fc7d003ca14
Mark Bevill <80132178+markbevill811@users.noreply.github.com>
2025-08-29 18:06:18 -0400

    Update SetJabraMicLevel.ps1

--- SetJabraMicLevel.ps1
+++ SetJabraMicLevel.ps1
@@ -1,13 +1,24 @@
 
 Import-Module AudioDeviceCmdlets
 
-# Get all capture (input) devices
+# Get all Jabra recording (input) devices
 $devices = Get-AudioDevice -List | Where-Object { $_.Type -eq 'Recording' -and $_.Name -like '*Jabra*' }
 
 $devices | ForEach-Object {
     Write-Host "Setting input level for: $($_.Index) - $($_.Name)"
-    $_ | Set-AudioDevice
+    $_ | Set-AudioDevice -Verbose
     Set-AudioDevice -RecordingMute 0
+    Set-AudioDevice -RecordingCommunicationMute 0
     Set-AudioDevice -RecordingVolume 100
     Set-AudioDevice -RecordingCommunicationVolume 100
+}
+
+# Get all Jabra playback (output) devices
+$devices = Get-AudioDevice -List | Where-Object { $_.Type -eq 'Playback' -and $_.Name -like '*Jabra*' }
+
+$devices | ForEach-Object {
+    Write-Host "Setting playback for: $($_.Index) - $($_.Name)"
+    $_ | Set-AudioDevice -Verbose
+    Set-AudioDevice -PlaybackMute 0
+    Set-AudioDevice -PlaybackCommunicationMute 0
 }
